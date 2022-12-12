d1f2a98198146e1daf6bb38ca4ab77b195227896
Smbrer1 <tumakaelfufaka@gmail.com>
2022-12-13 02:50:29 +0300

    Task 2

--- HHTask/Program.cs
+++ HHTask/Program.cs
@@ -1,14 +1,38 @@
-﻿using static AreaCalculator.Calculator;
+﻿using System.Data;
+using static AreaCalculator.Calculator;
+using Microsoft.Data.SqlClient;
+
 
 namespace HHTask
 {
     public static class Program
     {
+        private static SqlConnection SqlConnection()
+        {
+            const string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Task;Trusted_Connection=True;";
+
+            var connection = new SqlConnection(connectionString);
+            return connection;
+        }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(CalculatePolygonArea(10, 10));
-            Console.WriteLine(CalculateTriangleArea(10,5,1));
+            var connection = SqlConnection();
+            var sql = @"
+            SELECT pr.product_name, ca.category_name
+            FROM ""Product"" pr
+                LEFT OUTER JOIN product_category pc on pr.id = pc.product_id
+                LEFT OUTER JOIN ""Category"" ca
+                    ON pc.category_id = ca.id
+            ";
+            connection.Open();
+            using var cmd = new SqlCommand(sql, connection);
+            var result = cmd.ExecuteReader();
+            while (result.Read())
+            {
+                Console.WriteLine($"product:{result[0]} category:{result[1]}");
+            }
+            connection.Close();
         }
     }
 }
